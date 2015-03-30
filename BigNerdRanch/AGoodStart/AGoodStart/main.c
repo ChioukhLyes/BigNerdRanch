5574ca9b896ffe9a16007ac30a94657efc3df70e
Chioukh Lyes <l.chioukh@hotmail.fr>
2015-03-30 12:02:39 +0200

    feat(Project):Project creation

---
+++ BigNerdRanch/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Chioukh Lyes on 30/03/2015.
+//  Copyright (c) 2015 Excilys. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    //print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the wrong of times.\n");
+    
+    /*mlml
+     mlml
+     */
+    
+    return  0;
+}
